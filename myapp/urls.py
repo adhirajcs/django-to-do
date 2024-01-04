0a6ecbcaa34e15515c5c0ddf5da041b49e7d5e89
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-01-04 16:05:58 +0000

    Fixed a major bug

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.todos, name="home"),
-    path("todos/", views.todos, name="Todos"),
-    path("add_todos/", views.todos, name="addTodos")
+    # path("", views.todos, name="home"),
+    path("", views.todos, name="Todos")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import redirect, render, HttpResponse
 from .models import TodoItem
+from .forms import *
 
 # Create your views here.
 
-def home(request):
-    return render(request, "home.html")
-
-# def todos(request):
-#     items = TodoItem.objects.all()
-#     return render(request, "todos.html", {"todos": items })
+# def home(request):
+#     return render(request, "home.html")
 
 def todos(request):
     items = TodoItem.objects.all()
-    if request.method == 'POST':
-        new_todo_title = request.POST.get('newTodo')
-        if new_todo_title:
-            new_todo = TodoItem.objects.create(title=new_todo_title, completed=False)
-            new_todo.save()
-            redirect("todos.html")
-    return render(request, "todos.html", {"todos": items })
+    form = TodoForm()
 
-# def add_todos(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)        
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    return render(request, "todos.html", {"todos": items ,'form':form})
+
+
+# def todos(request):
+#     items = TodoItem.objects.all()
+#     form = TodoForm()
 #     if request.method == 'POST':
-#         new_todo_title = request.POST.get('newTodo')
+#         # new_todo_title = TodoForm(request.POST)
 #         if new_todo_title:
-#             new_todo = TodoItem.objects.create(title=new_todo_title, completed=False)
-#             new_todo.save()
-#             redirect("todos.html")
+#             # new_todo = TodoItem.objects.create(title=new_todo_title, completed=False)
+#             new_todo_title.save()
+#             redirect("/")
+    # return render(request, "todos.html", {"todos": items ,'form':form})
