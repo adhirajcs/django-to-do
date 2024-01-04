6a1ca001e878cf8d6077696ea63afa0f765470d0
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-01-04 10:56:13 +0000

    Added input box to add todos

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from .models import TodoItem
 
 # Create your views here.
@@ -6,6 +6,24 @@ from .models import TodoItem
 def home(request):
     return render(request, "home.html")
 
+# def todos(request):
+#     items = TodoItem.objects.all()
+#     return render(request, "todos.html", {"todos": items })
+
 def todos(request):
     items = TodoItem.objects.all()
+    if request.method == 'POST':
+        new_todo_title = request.POST.get('newTodo')
+        if new_todo_title:
+            new_todo = TodoItem.objects.create(title=new_todo_title, completed=False)
+            new_todo.save()
+            redirect("todos.html")
     return render(request, "todos.html", {"todos": items })
+
+# def add_todos(request):
+#     if request.method == 'POST':
+#         new_todo_title = request.POST.get('newTodo')
+#         if new_todo_title:
+#             new_todo = TodoItem.objects.create(title=new_todo_title, completed=False)
+#             new_todo.save()
+#             redirect("todos.html")
